2f55eac4bdba21421b2907349096739a14960a70
macor001 <macorhua@gmail.com>
2020-01-18 22:50:30 +0800

    添加等待-通知机制的转账

--- src/macor/algorithm/demo/Allocator.java
+++ src/macor/algorithm/demo/Allocator.java
@@ -3,6 +3,9 @@ package macor.algorithm.demo;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 此转账适合并发冲突量不大时的场合，因为在大数据量环境下，循环等待这种方案就不适用了，性能太差
+ */
 public class Allocator {
     private List<Object> als = new ArrayList<>();
 
---
+++ src/macor/algorithm/demo/NotifyAllocator.java
@@ -0,0 +1,30 @@
+package macor.algorithm.demo;
+
+import java.util.List;
+
+/**
+ * 此转账适合并发冲突量不大时的场合，因为在大数据量环境下，通过等待-通知机制可以提高性能
+ */
+public class NotifyAllocator {
+    private List<Object> als;
+
+    synchronized void apply(Object from, Object to){
+        while (als.contains(from) || als.contains(to)){
+            try{
+                wait();
+            }
+            catch (Exception e){
+
+            }
+            als.add(from);
+            als.add(to);
+        }
+    }
+
+    synchronized void free(Object from, Object to){
+        als.remove(from);
+        als.remove(to);
+        notifyAll();
+    }
+
+}
