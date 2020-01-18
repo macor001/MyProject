0061042bde0ccd79ccda4f795f90a0280d90112b
macor001 <macorhua@gmail.com>
2020-01-18 20:33:26 +0800

    添加并发实战缓存导致的可见性问题

---
+++ src/macor/algorithm/demo/Calculate.java
@@ -0,0 +1,26 @@
+package macor.algorithm.demo;
+
+public class Calculate {
+    private long count = 0;
+    private void add(int number){
+        int idx = 0;
+        while (idx ++ < number){
+            count += 1;
+        }
+    }
+
+    public static long calc(int number){
+        final Calculate cal = new Calculate();
+        Thread thread1 = new Thread(()->{cal.add(number);});
+        Thread thread2 = new Thread(()->{cal.add(number);});
+        thread1.start();
+        thread2.start();
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return cal.count;
+    }
+}
---
+++ src/macor/algorithm/demo/Main.java
@@ -0,0 +1,9 @@
+package macor.algorithm.demo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // write your code here
+        Calculate.calc(100000);
+    }
+}
